b2707d86346f85bf1944d51a0ba2d2fa1f8159c4
Tran-Trung-Nhut <ttnhut904@gmail.com>
2025-09-03 23:48:24 +0700

    update loading state for some page

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -7,9 +7,11 @@ import CartItemCard from "../components/CartItemCard"
 import cartItemApi from "../apis/cartItemApi"
 import { HttpStatusCode } from "axios"
 import { useNavigate } from "react-router-dom"
+import LoadingAuth from "../components/LoadingAuth"
 
 const CartPage = () => {
   const [cart, setCart] = useState<Cart | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
   const { userInfo } = useAuth()
   const didRun = useRef(false);
   const token = getGuestToken()
@@ -17,12 +19,16 @@ const CartPage = () => {
   const navigate = useNavigate()
 
   const removeFromCart = async (id: number) => {
+    setLoading(true)
     const result = await cartItemApi.removeFromCart(id)
 
-    if (result.status !== HttpStatusCode.Ok) return notifyError('Đã có lỗi xảy ra. Vui lòng thử lại')
+    if (result.status !== HttpStatusCode.Ok) {
+      setLoading(false)
+      return notifyError('Đã có lỗi xảy ra. Vui lòng thử lại')
+    }
 
     setCart(prevCart => {
-      if (!prevCart) return prevCart; // nếu cart = null thì bỏ qua
+      if (!prevCart) return prevCart
 
       const updatedCartItems = prevCart.cartItems.filter(item => item.id !== id);
 
@@ -38,6 +44,7 @@ const CartPage = () => {
 
       return updatedCart;
     });
+    setLoading(false)
     notifySuccess('Đã xóa sản phẩm khỏi giỏ hàng')
   }
 
@@ -47,19 +54,25 @@ const CartPage = () => {
   }
 
   const getCart = async () => {
+    setLoading(true)
     try {
       if (userInfo) {
         const result = await cartApi.getCartItemsByUserId(userInfo.id)
 
-        if (result.status !== HttpStatusCode.Ok) return
+        if (result.status !== HttpStatusCode.Ok) return setLoading(false)
         setCart(result.data)
 
       } else {
-        if (!token) return notifyError("Có lỗi xảy ra. Vui lòng thử lại")
+        if (!token) {
+          setLoading(false)
+          return notifyError("Có lỗi xảy ra. Vui lòng thử lại")
+        }
         setCart((await cartApi.getCartByGuestToken(token)).data)
       }
     } catch (error) {
       console.error(error)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -70,39 +83,41 @@ const CartPage = () => {
     }
   }, [])
   return (
-    <div className="container mx-auto px-4 py-8 min-h-[600px]">
-      <h1 className="text-3xl font-bold text-center text-sky-500 mb-4">Giỏ Hàng Của Bạn</h1>
-      <div className="text-gray-500 mb-4 pb-2 border-b-2">
-        <a href="/">Trang chủ</a> / Giỏ hàng ({cart?.cartItems.length || 0})
-      </div>
-      <div className="flex flex-col md:flex-row gap-8">
-        <div className="md:w-2/3">
-          <div className="space-y-4">
-            {cart?.cartItems.map(cartItem => (
-              <CartItemCard key={cartItem.id} cartItem={cartItem} removeFromCart={removeFromCart} />
-            ))}
-          </div>
+    loading ? <LoadingAuth /> : (
+      <div className="container mx-auto px-4 py-8 min-h-[600px]">
+        <h1 className="text-3xl font-bold text-center text-sky-500 mb-4">Giỏ Hàng Của Bạn</h1>
+        <div className="text-gray-500 mb-4 pb-2 border-b-2">
+          <a href="/">Trang chủ</a> / Giỏ hàng ({cart?.cartItems.length || 0})
         </div>
-        <div className="md:w-1/3">
-          <div className="order-summary p-4 rounded-md">
-            <h2 className="text-xl font-bold text-black mb-4">Thông Tin Đơn Hàng</h2>
-            <div className="flex justify-between mb-2">
-              <span className="font-bold text-gray-800">Tổng tiền:</span>
-              <span className="text-red-500 font-bold">{formatPrice(cart?.totalAmount || 0)}</span>
+        <div className="flex flex-col md:flex-row gap-8">
+          <div className="md:w-2/3">
+            <div className="space-y-4">
+              {cart?.cartItems.map(cartItem => (
+                <CartItemCard key={cartItem.id} cartItem={cartItem} removeFromCart={removeFromCart} />
+              ))}
             </div>
-            <ul className="list-disc list-inside text-gray-800 mb-4">
-              <li>Phí vận chuyển sẽ được tính ở trang thanh toán.</li>
-              <li>Bạn cũng có thể nhập mã giảm giá ở trang thanh toán.</li>
-            </ul>
-            <button
-              className="checkout-btn w-full bg-sky-500 text-white py-2 rounded-md font-semibold flex items-center justify-center hover:bg-sky-600"
-              onClick={() => handleCheckout()}>
-              THANH TOÁN
-            </button>
+          </div>
+          <div className="md:w-1/3">
+            <div className="order-summary p-4 rounded-md">
+              <h2 className="text-xl font-bold text-black mb-4">Thông Tin Đơn Hàng</h2>
+              <div className="flex justify-between mb-2">
+                <span className="font-bold text-gray-800">Tổng tiền:</span>
+                <span className="text-red-500 font-bold">{formatPrice(cart?.totalAmount || 0)}</span>
+              </div>
+              <ul className="list-disc list-inside text-gray-800 mb-4">
+                <li>Phí vận chuyển sẽ được tính ở trang thanh toán.</li>
+                <li>Bạn cũng có thể nhập mã giảm giá ở trang thanh toán.</li>
+              </ul>
+              <button
+                className="checkout-btn w-full bg-sky-500 text-white py-2 rounded-md font-semibold flex items-center justify-center hover:bg-sky-600"
+                onClick={() => handleCheckout()}>
+                THANH TOÁN
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   )
 }
 
--- frontend/src/pages/ProductDetailPage.tsx
+++ frontend/src/pages/ProductDetailPage.tsx
@@ -8,8 +8,10 @@ import cartItemApi from "../apis/cartItemApi";
 import { Minus, Plus } from "lucide-react";
 import { HttpStatusCode } from "axios";
 import type { ProductVariant } from "../dtos/productVariant.dto";
+import LoadingAuth from "../components/LoadingAuth";
 
 const ProductDetailPage = () => {
+    const [loading, setLoading] = useState<boolean>(false)
     const [product, setProduct] = useState<ProductDetail | null>(null);
     const [listSize, setListSize] = useState<string[]>([])
     const [listColor, setListColor] = useState<string[]>([])
@@ -22,18 +24,32 @@ const ProductDetailPage = () => {
     const navigate = useNavigate()
 
     const addToCart = async () => {
-        if (!product) return notifyError("Không tìm thấy sản phẩm trong cơ sở dữ liệu. Vui lòng thử lại sau")
+        setLoading(true)
+        if (!product) {
+            setLoading(false)
+            return notifyError("Không tìm thấy sản phẩm trong cơ sở dữ liệu. Vui lòng thử lại sau")
+        }
 
         const guestToken = getGuestToken()
-        if (!guestToken) return notifyError("Có lỗi xảy ra. Vui lòng thử lại sau")
+        if (!guestToken) {
+            setLoading(false)
+            return notifyError("Có lỗi xảy ra. Vui lòng thử lại sau")
+        }
 
         const variant = getVariant(chosenColor, chosenSize)
-        if (!variant) return notifyError("Không tìm thấy màu sắc và kích thước này trong cơ sở dữ liệu. Vui lòng thử lại")
+        if (!variant) {
+            setLoading(false)
+            return notifyError("Không tìm thấy màu sắc và kích thước này trong cơ sở dữ liệu. Vui lòng thử lại")
+        }
 
         const response = await cartItemApi.addItemToCart({ guestToken, priceAtAdding: variant.price, quantity, productId: product?.id, productVariantId: variant.id })
-        
-        if (response.status !== HttpStatusCode.Created) return notifyWarning(response.data.message)
-            
+
+        if (response.status !== HttpStatusCode.Created) {
+            setLoading(false)
+            return notifyWarning(response.data.message)
+        }
+
+        setLoading(false)
         notifySuccess("Đã thêm sản phẩm vào giỏ hàng")
     };
 
@@ -44,6 +60,7 @@ const ProductDetailPage = () => {
     }
 
     const fetchProductDetail = async () => {
+        setLoading(true)
         const data = (await productApi.getProduct(Number(id))).data.data
         setProduct(data)
 
@@ -55,6 +72,8 @@ const ProductDetailPage = () => {
             setListSize(Array.from(new Set(data.variants.map((variant: ProductVariant) => variant.size))))
             setListColor(Array.from(new Set(data.variants.map((variant: ProductVariant) => variant.color))))
         }
+
+        setLoading(false)
     }
 
     const updatePriceByChosenColorAndSize = (colorOrSizeorQuatity: string, value: string) => {
@@ -102,133 +121,134 @@ const ProductDetailPage = () => {
     };
 
     return (
-        product ? (
-            <section className="p-5 min-h-[500px]">
-                <nav className="text-sm text-gray-500 mb-4">
-                    <a href="/" className="hover:underline">Trang chủ </a>/
-                    <a href="/shop" className="hover:underline"> Cửa hàng </a>/
-                    <span> {product.name}</span>
-                </nav>
-                <div className="flex flex-col md:flex-row">
-                    <div className="md:w-1/2 flex justify-center">
+        loading ? <LoadingAuth /> :
+            product ? (
+                <section className="p-5 min-h-[500px]">
+                    <nav className="text-sm text-gray-500 mb-4">
+                        <a href="/" className="hover:underline">Trang chủ </a>/
+                        <a href="/shop" className="hover:underline"> Cửa hàng </a>/
+                        <span> {product.name}</span>
+                    </nav>
+                    <div className="flex flex-col md:flex-row">
                         <div className="md:w-1/2 flex justify-center">
-                            {product.image.length > 0 ? (
-                                product.image.length === 1 ? (
+                            <div className="md:w-1/2 flex justify-center">
+                                {product.image.length > 0 ? (
+                                    product.image.length === 1 ? (
+                                        <img
+                                            src={product.image[0]}
+                                            alt="Product image"
+                                            className="w-52 h-auto object-cover"
+                                        />
+                                    ) : (
+                                        <Slider {...settings} className="w-52">
+                                            {product.image.map((img, index) => (
+                                                <div key={index}>
+                                                    <img
+                                                        src={img}
+                                                        alt={`Product image ${index + 1}`}
+                                                        className="w-full h-auto object-cover"
+                                                    />
+                                                </div>
+                                            ))}
+                                        </Slider>
+                                    )
+                                ) : (
                                     <img
-                                        src={product.image[0]}
-                                        alt="Product image"
+                                        src="path/to/default-image.jpg"
+                                        alt="Product"
                                         className="w-52 h-auto object-cover"
                                     />
-                                ) : (
-                                    <Slider {...settings} className="w-52">
-                                        {product.image.map((img, index) => (
-                                            <div key={index}>
-                                                <img
-                                                    src={img}
-                                                    alt={`Product image ${index + 1}`}
-                                                    className="w-full h-auto object-cover"
-                                                />
-                                            </div>
-                                        ))}
-                                    </Slider>
-                                )
-                            ) : (
-                                <img
-                                    src="path/to/default-image.jpg"
-                                    alt="Product"
-                                    className="w-52 h-auto object-cover"
-                                />
-                            )}
+                                )}
+                            </div>
                         </div>
-                    </div>
-                    <div className="md:w-1/2 md:pl-10 mt-6 md:mt-0">
-                        <h1 className="text-3xl font-bold uppercase text-black">{product.name}</h1>
-                        <p className="text-xl text-black mt-2">{formatPrice(price)}</p>
+                        <div className="md:w-1/2 md:pl-10 mt-6 md:mt-0">
+                            <h1 className="text-3xl font-bold uppercase text-black">{product.name}</h1>
+                            <p className="text-xl text-black mt-2">{formatPrice(price)}</p>
 
-                        {/* Chọn Size */}
-                        {listSize[0] !== 'no_size' && (
+                            {/* Chọn Size */}
+                            {listSize[0] !== 'no_size' && (
+                                <div className="mt-6">
+                                    <h3 className="text-sm font-semibold text-gray-600 mb-2">Chọn size</h3>
+                                    <div className="flex space-x-3">
+                                        {listSize.map((size) => (
+                                            <button
+                                                key={size}
+                                                className={`px-4 py-2 border rounded-lg ${chosenSize === size ? "bg-sky-500 text-white" : "hover:border-sky-500 hover:text-sky-500"}`}
+                                                onClick={() => handleChooseSize(size)}
+                                            >
+                                                {size}
+                                            </button>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+
+                            {/* Chọn Màu */}
                             <div className="mt-6">
-                                <h3 className="text-sm font-semibold text-gray-600 mb-2">Chọn size</h3>
+                                <h3 className="text-sm font-semibold text-gray-600 mb-2">Chọn màu</h3>
                                 <div className="flex space-x-3">
-                                    {listSize.map((size) => (
+                                    {listColor.map((color) => (
                                         <button
-                                            key={size}
-                                            className={`px-4 py-2 border rounded-lg ${chosenSize === size ? "bg-sky-500 text-white" : "hover:border-sky-500 hover:text-sky-500"}`}
-                                            onClick={() => handleChooseSize(size)}
+                                            key={color}
+                                            className={`px-4 py-2 border rounded-lg ${chosenColor === color ? "bg-sky-500 text-white" : "hover:border-sky-500 hover:text-sky-500"}`}
+                                            onClick={() => handleChooseColor(color)}
                                         >
-                                            {size}
+                                            {color}
                                         </button>
                                     ))}
                                 </div>
                             </div>
-                        )}
 
-                        {/* Chọn Màu */}
-                        <div className="mt-6">
-                            <h3 className="text-sm font-semibold text-gray-600 mb-2">Chọn màu</h3>
-                            <div className="flex space-x-3">
-                                {listColor.map((color) => (
+                            <div className="mt-6">
+                                <h3 className="text-sm font-semibold text-gray-600 mb-2">Số lượng</h3>
+                                <div className="flex space-x-3">
                                     <button
-                                        key={color}
-                                        className={`px-4 py-2 border rounded-lg ${chosenColor === color ? "bg-sky-500 text-white" : "hover:border-sky-500 hover:text-sky-500"}`}
-                                        onClick={() => handleChooseColor(color)}
-                                    >
-                                        {color}
+                                        type="button"
+                                        onClick={() => quantity > 0 && handleChangeQuantity(quantity - 1)}>
+                                        <Minus size={16} />
                                     </button>
-                                ))}
+                                    <input
+                                        type="number"
+                                        className={`px-4 py-2 w-12 border rounded-lg text-center text-xl [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none`}
+                                        value={quantity}
+                                        onChange={(e) => setQuantity(Number(e.target.value))}
+                                    />
+                                    <button
+                                        type="button"
+                                        onClick={() => handleChangeQuantity(quantity + 1)}>
+                                        <Plus size={16} />
+                                    </button>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="mt-6">
-                            <h3 className="text-sm font-semibold text-gray-600 mb-2">Số lượng</h3>
-                            <div className="flex space-x-3">
+                            {/* Nút hành động */}
+                            <div className="mt-6 flex space-x-4">
                                 <button
-                                    type="button"
-                                    onClick={() => quantity > 0 && handleChangeQuantity(quantity - 1)}>
-                                    <Minus size={16} />
+                                    className="bg-sky-500 text-white px-6 py-2 rounded-lg hover:opacity-80"
+                                    onClick={() => navigate(`/checkout`, { state: { product, productVariant: getVariant(chosenColor, chosenSize), quantity } })}>
+                                    MUA NGAY
                                 </button>
-                                <input
-                                    type="number"
-                                    className={`px-4 py-2 w-12 border rounded-lg text-center text-xl [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none`}
-                                    value={quantity}
-                                    onChange={(e) => setQuantity(Number(e.target.value))}
-                                />
                                 <button
-                                    type="button"
-                                    onClick={() => handleChangeQuantity(quantity + 1)}>
-                                    <Plus size={16} />
+                                    className="bg-white text-sky-500 border-2 border-sky-500 px-6 py-2 rounded-lg hover:bg-gray-200"
+                                    onClick={() => addToCart()}
+                                >
+                                    THÊM VÀO GIỎ HÀNG
                                 </button>
                             </div>
-                        </div>
 
-                        {/* Nút hành động */}
-                        <div className="mt-6 flex space-x-4">
-                            <button
-                                className="bg-sky-500 text-white px-6 py-2 rounded-lg hover:opacity-80"
-                                onClick={() => navigate(`/checkout`, { state: { product, productVariant: getVariant(chosenColor, chosenSize), quantity } })}>
-                                MUA NGAY
-                            </button>
-                            <button
-                                className="bg-white text-sky-500 border-2 border-sky-500 px-6 py-2 rounded-lg hover:bg-gray-200"
-                                onClick={() => addToCart()}
-                            >
-                                THÊM VÀO GIỎ HÀNG
-                            </button>
-                        </div>
-
-                        {/* Description nằm dưới cùng */}
-                        <div className="mt-8">
-                            <h3 className="text-lg font-semibold text-gray-700">Thông tin sản phẩm</h3>
-                            <p className="text-base text-black mt-2 leading-relaxed">
-                                {product.description}
-                            </p>
+                            {/* Description nằm dưới cùng */}
+                            <div className="mt-8">
+                                <h3 className="text-lg font-semibold text-gray-700">Thông tin sản phẩm</h3>
+                                <p className="text-base text-black mt-2 leading-relaxed">
+                                    {product.description}
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        ) : (
-            <h1>Không tìm thấy sản phẩm</h1>
-        )
+                </section>
+            ) : (
+                <h1>Không tìm thấy sản phẩm</h1>
+            )
     );
 };
 
